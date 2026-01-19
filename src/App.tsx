d809bbc26a5821b264801f9c86b9eb50c189d497
Ben Kile <benjaminfkile@gmail.com>
2026-01-18 22:03:06 -0600

    clean up unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { JSX, useState } from "react";
+import { JSX } from "react";
 import WorldScene from "./WorldScene";
-import "./App.css"
+import "./App.css";
 
 export default function App(): JSX.Element {
-  const [coords, setCoords] = useState({ latitude: 46.8721, longitude: -113.994 });
-
   return (
     <div className="App">
-      <WorldScene onCoordsUpdate={setCoords} />
+      <WorldScene />
     </div>
-  )
+  );
 }
